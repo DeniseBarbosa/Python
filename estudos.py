a026341488301eeb6526e7c7b7b062f7e321e130
DeniseBarbosa <deniseluvoir@gmail.com>
2022-07-11 18:01:54 -0300

    Variáveis e Keywords

--- estudos.py
+++ estudos.py
@@ -1,5 +1,38 @@
-#Executando o Primeiro Código
-nome = 'Denise Prinsloo'
-print('Olá', nome, 'Seja Bem vinda')
 
+# A criação de uma variável é realizada por meio da declaração e a atribuição de um valor à ela:
+#
+# variavel = valor
 
+pessoa = 'Denise Pereira'
+idade = 29
+altura = 1.70
+print(pessoa, idade, altura)
+
+texto = 'Este é um texto grande Este é um texto grande Este é um texto grande Este é um texto grande Este é um texto grande Este é um texto grande Este é um texto grande Este é um texto grande Este é um texto grande Este é um texto grande'
+porcentagem = 0.5
+numero = -100
+print(numero)
+
+idade = 2022 - 1992
+print(idade)
+
+decimal = 15 / 4
+print(decimal)
+
+variavel = 0
+var22iavel = 99
+print(variavel, var22iavel)
+
+NOME = 'Denise'
+Sobrenome = 'Prinsloo'
+print(NOME, Sobrenome)
+
+nome = 'João'
+Nome = 'Luiz'
+print(nome, NOME, Nome)
+
+_valor_total_ = 1000
+print(_valor_total_)
+
+FOR = 32
+print(FOR)
